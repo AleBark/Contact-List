d9d47702b84fc9b9dfd5ab0f92d2c45a16992a85
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-08-15 23:50:32 -0300

    editing, and saving new contacts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:contact_list_app/ui/contact_page.dart';
 import 'package:contact_list_app/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    debugShowCheckedModeBanner: false,
 
   ));
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,25 +3,21 @@ import 'dart:io';
 import 'package:contact_list_app/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
+import 'contact_page.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
   List<Contact> contacts = List();
-
 
   @override
   void initState() {
     super.initState();
-    helper.getAllContacts().then((list){
-      setState(() {
-        contacts = list;
-      });
-    });
+    _getAllContacts();
   }
 
   @override
@@ -33,54 +29,90 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       backgroundColor: Colors.white,
-      floatingActionButton: FloatingActionButton(onPressed: (){},
-        child: Icon(Icons.add) ,
-        backgroundColor: Colors.red,),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showContactPage();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
       body: ListView.builder(
           padding: EdgeInsets.all(10.0),
           itemCount: contacts.length,
-          itemBuilder: (context, index){
+          itemBuilder: (context, index) {
             return _contactCard(context, index);
-          }
-      ),
+          }),
     );
   }
 
-  Widget _contactCard(BuildContext context, int index){
+  Widget _contactCard(BuildContext context, int index) {
     return GestureDetector(
       child: Card(
         child: Padding(
-            padding: EdgeInsets.all(10.0),
-                child: Row(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 58.0,
+                height: 58.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: contacts[index].img != null
+                            ? FileImage(File(contacts[index].img))
+                            : AssetImage("images/person.png"))),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Container(
-                      width: 58.0,
-                      height: 58.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: contacts[index].img != null ?
-                                FileImage(File(contacts[index].img)) :
-                                AssetImage("images/person.png")
-                        )
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(contacts[index].name ?? "",style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)),
-                          Text(contacts[index].email ?? "",style: TextStyle(fontSize: 18.0)),
-                          Text(contacts[index].phone ?? "",style: TextStyle(fontSize: 18.0)),
-                        ],
-                      ),
-                    )
+                    Text(contacts[index].name ?? "",
+                        style: TextStyle(
+                            fontSize: 22.0, fontWeight: FontWeight.bold)),
+                    Text(contacts[index].email ?? "",
+                        style: TextStyle(fontSize: 18.0)),
+                    Text(contacts[index].phone ?? "",
+                        style: TextStyle(fontSize: 18.0)),
                   ],
-        ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        _showContactPage(contact: contacts[index]);
+      },
     );
   }
 
+  void _getAllContacts() {
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  void _showContactPage({Contact contact}) async {
+    final recContact = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ContactPage(
+                  contact: contact,
+                )));
+
+    //if any contact is returned
+    if (recContact != null) {
+//      if user sent a contact
+      if (contact != null) {
+        await helper.updateContact(recContact);
+      } else {
+        //user do not sent any contact, but a contact was returned (new record), save it.
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
+  }
 }
